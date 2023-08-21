6847925f189fcc55b935c004793691d61d0ab2c3
Joshua Kunst <jbkunst@gmail.com>
2023-08-21 11:49:00 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,77 +6,65 @@ page_navbar(
   title = tags$span(
     class = "ttl",
     tags$img(src = "logo.png", width = "46px", height = "auto", class = "me-3"),
-    "FNDR-App"
+    tags$span("FNDR", tags$sup("APP"))
   ),
   sidebar = sidebar(width = 300, sidebar_content),
   nav_panel(
     title = "Inicio",
     icon  = icon("dashboard"),
-    # layout_column_wrap(
-      # width = 1/2,
-    layout_columns(
-      col_widths = c(5, 7),
-      # fillable = TRUE,
-      leafletOutput("mapa_main"),
-      layout_column_wrap(
-        1/3,
-        fillable = TRUE,
-        fill = TRUE,
-        # data |> count(tipologia_dentro_del_eje, sort = TRUE) |> View()
-        value_box(NULL, uiOutput("hero_aceras"), "Aceras"),
-        value_box(NULL, uiOutput("hero_pavcal"), "Pavimentacion De Calzadas"),
-        value_box(NULL, uiOutput("hero_refptn"), "Refugios Peatonales"),
-        value_box(NULL, uiOutput("hero_ciclov"), "Ciclovias"),
-        value_box(NULL, uiOutput("hero_lumina"), "Luminarias"),
-        value_box(NULL, uiOutput("hero_alarms"), "Alarmas")
-        # value_box(NULL, uiOutput("hero_bacheo"), "Bacheo De Calzadas"),
+    layout_column_wrap(
+      width = 1/4,
+      fillable = TRUE,
+      fill = TRUE,
+      # data |> count(tipologia_dentro_del_eje, sort = TRUE) |> View()
+      value_box(NULL, uiOutput("hero_aceras"), "Aceras"),
+      value_box(NULL, uiOutput("hero_pavcal"), "Pavimentacion De Calzadas"),
+      value_box(NULL, uiOutput("hero_refptn"), "Refugios Peatonales"),
+      value_box(NULL, uiOutput("hero_ciclov"), "Ciclovias"),
+      value_box(NULL, uiOutput("hero_lumina"), "Luminarias"),
+      # value_box(NULL, uiOutput("hero_bacheo"), "Bacheo De Calzadas"),
+      value_box(NULL, uiOutput("hero_alarms"), "Alarmas")
+      )
+    ),
+  nav_panel(
+    title = "Gráficos",
+    icon = icon("chart-pie"),
+    navset_card_tab(
+      title = NULL,
+      nav_panel(
+        title = "Cantidad Iniciativas",
+        layout_column_wrap(
+         1/2,
+         fillable = TRUE,
+         fill = TRUE,
+         card(highchartOutput("chart_proy_prov_comuna")),
+         card(highchartOutput("chart_proy_eje_area")),
+         card(highchartOutput("chart_tipologia")),
+         card(highchartOutput("chart_tipologia_dentro_eje"))
+         )
+        ),
+      nav_panel(
+        title = "Monto (millones $)",
+        layout_column_wrap(
+          1/2,
+          fillable = TRUE,
+          fill = TRUE,
+          card(highchartOutput("chart_proy_prov_comuna_m")),
+          card(highchartOutput("chart_proy_eje_area_m")),
+          card(highchartOutput("chart_tipologia_m")),
+          card(highchartOutput("chart_tipologia_dentro_eje_m"))
+          )
         )
       )
     ),
   nav_panel(
-   title = "Gráficos",
-   icon = icon("chart-pie"),
-
-   navset_card_tab(
-     title = NULL,
-     nav_panel(
-       title = "Cantidad Iniciativas",
-       layout_column_wrap(
-         1/2,
-         fillable = TRUE,
-         fill = TRUE,
-         card(highchartOutput("home_chart_proy_sector")),
-         card(highchartOutput("home_chart_proy_eje")),
-         card(highchartOutput("home_chart_proy_prov")),
-         card(highchartOutput("home_chart_etapa_anio"))
-         )
-       ),
-     nav_panel(
-       title = "Monto (millones $)",
-       layout_column_wrap(
-         1/2,
-         fillable = TRUE,
-         fill = TRUE,
-         card(highchartOutput("home_chart_proy_sector_m")),
-         card(highchartOutput("home_chart_proy_eje_m")),
-         card(highchartOutput("home_chart_proy_prov_m")),
-         card(highchartOutput("home_chart_etapa_anio_m"))
-       )
-     ),
-     nav_panel(
-       title = "Otra sección",
-     )
-     )
-   ),
-  nav_panel(
-    title = "Iniciativas",
+    title = "Listado Iniciativas",
     icon  = icon("table"),
     DT::dataTableOutput("tabla_main")
+    ),
+  nav_panel(
+    title = "Mapa inciativas",
+    icon  = icon("map"),
+    leafletOutput("mapa_main")
+    )
   )
-  # nav_panel(
-  #   title = "Acerca de",
-  #   icon  = icon("question-circle"),
-  #   "No tenemos mucha información..."
-  #   # DT::dataTableOutput("tabla_main")
-  # )
-)
